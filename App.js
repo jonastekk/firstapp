ddb885f97d75bf00eba4f7c83031c1ee4fd0c865
jonastekk <44065469+jonastekk@users.noreply.github.com>
2019-08-19 17:29:52 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import HomeScreen from "./screens/HomeScreen";
+import AppNavigator from "./navigator/AppNavigator";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+
+const client = new ApolloClient({
+  uri: `https://graphql.contentful.com/content/v1/spaces/ldcl3ayg0mhx`,
+  credentials: "same-origin",
+  headers: {
+    Authorization: `Bearer 93f3808c25c1f5bdb95476ca8576c6eaa12b5587efb956efb242ceead7cb3840`
+  }
+});
+
+const initialState = {
+  action: "",
+  name: ""
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "OPEN_MENU":
+      return { action: "openMenu" };
+    case "CLOSE_MENU":
+      return { action: "closeMenu" };
+    case "UPDATE_NAME":
+      return { name: action.name };
+    case "OPEN_CARD":
+      return { action: "openCard" };
+    case "CLOSE_CARD":
+      return { action: "closeCard" };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+const App = () => {
+  return (
+    <ApolloProvider client={client}>
+      <Provider store={store}>
+        <AppNavigator />
+      </Provider>
+    </ApolloProvider>
+  );
+};
+
+export default App;
